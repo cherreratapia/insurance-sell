0978a60e5363446e9a15c237c4fb3e9d0d66eb26
Cristopher Herrera <c.herreratapia@hotmail.com>
2019-12-02 21:36:42 -0300

    test para agregar un nuevo item de seguro

--- src/daos/Insurance/InsuranceDao.test.ts
+++ src/daos/Insurance/InsuranceDao.test.ts
@@ -9,4 +9,23 @@ describe("Insurance Data-Access-Object test", () => {
       expect(result).toEqual([]);
     });
   });
+  describe("Add a new insurance item to the array", () => {
+    const insuranceDao = new InsuranceDao();
+    const newInsurance = {
+      name: "test",
+      price: 100,
+      sellIn: 10
+    };
+    const insurance: Insurance = new Insurance(newInsurance);
+    const result = insuranceDao.add(insurance);
+    expect(result).toEqual([
+      {
+        name: "test",
+        price: 100,
+        sellIn: 10,
+        rule: [],
+        effect: []
+      }
+    ]);
+  });
 });
